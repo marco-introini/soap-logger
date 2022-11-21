e2342fa70797888cbf2d8487cdcf521de4854568
Marco Introini <89445565+marco-introini@users.noreply.github.com>
2022-11-21 15:36:18 +0100

    first test

--- src/XmlLogger.php
+++ src/XmlLogger.php
@@ -3,6 +3,7 @@
 namespace Mintdev\Xml\Logger;
 
 use Carbon\Carbon;
+use Mintdev\Xml\Logger\Types\Step;
 use Monolog\Formatter\FormatterInterface;
 use Monolog\Handler\RotatingFileHandler;
 use Monolog\Handler\StreamHandler;
@@ -24,45 +25,22 @@ class XmlLogger
         $this->logger->pushHandler($handler);
     }
 
-    private function data(): string
+    private function date(): string
     {
         return Carbon::now()->format('d-j-Y G:i:s:v');
     }
 
-    public function logInputXml(): void
+    public function logGenerico(string $message, Step $step): void
     {
-        $request = file_get_contents('php://input');
         $soapAction = $_SERVER['HTTP_SOAPACTION'] ?? "n/a";
 
-        $this->logger->info("Request", [
-            'data' => $this->data(),
-            'servizio' => $this->serviceName,
-            'metodo' => $this->methodName ?? "n/a",
+        $this->logger->info($step->value, [
+            'date' => $this->date(),
+            'serviceName' => $this->serviceName,
             'soapAction' => $soapAction,
-            'step' => 'FROM_CALLER_TO_SERVICE',
-            'messaggio' => $request,
-        ]);
-    }
-
-    public function logOutputXml(string $response): void
-    {
-        $this->logger->info("Response", [
-            'data' => $this->data(),
-            'servizio' => $this->serviceName,
-            'metodo' => $this->methodName ?? "n/a",
-            'step' => 'FROM_SERVICE_TO_CALLER',
-            'messaggio' => $response,
-        ]);
-    }
-
-    public function logGenerico(string $message, $step): void
-    {
-        $this->logger->info($step, [
-            'data' => $this->data(),
-            'servizio' => $this->serviceName,
-            'metodo' => $this->methodName ?? "n/a",
-            'step' => $step,
-            'messaggio' => $message,
+            'methodName' => $this->methodName ?? "n/a",
+            'step' => $step->value,
+            'message' => $message,
         ]);
     }
 
---
+++ tests/XmlLoggerTest.php
@@ -0,0 +1,5 @@
+<?php
+
+use function Pest\Faker\faker;
+
+
